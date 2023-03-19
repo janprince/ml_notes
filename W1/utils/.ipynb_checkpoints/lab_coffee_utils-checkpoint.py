a1b58fe98b03345bc2faac7c5ca326d85e2cae33
Prince Jan Kumi <janprince002@gmail.com>
2023-03-19 08:33:51 +0000

    add numpy model

--- W1/utils/.ipynb_checkpoints/lab_coffee_utils-checkpoint.py
+++ W1/utils/.ipynb_checkpoints/lab_coffee_utils-checkpoint.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# plt.style.use('deeplearning.mplstyle')
+# plt.style.use('./deeplearning.mplstyle')
 import tensorflow as tf
 from tensorflow.keras.activations import sigmoid
 from matplotlib import cm
 import matplotlib.colors as colors
-# from lab_utils_common import dlc
+from lab_utils_common import dlc
 
 def load_coffee_data():
     """ Creates a coffee roasting data set.
